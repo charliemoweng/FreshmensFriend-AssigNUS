eb1c006e25a214442e3c5daf6f72678f5ccbde95
charliemoweng <68073477+charliemoweng@users.noreply.github.com>
2021-05-28 17:54:12 +0800

    Firebase config Google FIrebase configs

---
+++ src/config/firebase.js
@@ -0,0 +1,8 @@
+export const config = {
+  apiKey: "AIzaSyCC4ZjSgwQp2V7vwjTe483dj4NqJ4tkfus",
+  authDomain: "freshmen-s-friend-assignus.firebaseapp.com",
+  projectId: "freshmen-s-friend-assignus",
+  storageBucket: "freshmen-s-friend-assignus.appspot.com",
+  messagingSenderId: "942544328689",
+  appId: "1:942544328689:web:239c0cc7c4e202bc671650"
+};
