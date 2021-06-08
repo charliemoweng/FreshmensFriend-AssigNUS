a00b24ac81a4fa7d50fd7928e86ca8991af11a60
charliemoweng <68073477+charliemoweng@users.noreply.github.com>
2021-06-08 17:27:42 +0800

    Added in My Modules section, replaced Calendar image with Base Calendar

---
+++ src/components/TaskManager/MyModules/MyModules.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import Typography from "@material-ui/core/Typography";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%"
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
+    textAlign: "center"
+  }
+}));
+
+export default function MyModules() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography className={classes.heading}>
+            <b>Current Modules</b>
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>Add your Module.</Typography>
+        </AccordionDetails>
+      </Accordion>
+    </div>
+  );
+}
